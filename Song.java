061f566934c2c3da941c25c528bdff9f1bb10824
ShauryaMishra-Lab <Shauryakumarmishra@gmail.com>
2026-02-10 20:35:22 -0800

    Create Song class with title and rating functionality
    Implement Song class with title and rating attributes, including methods for rating adjustment and string representation.

---
+++ Song.java
@@ -0,0 +1,48 @@
+public class Song
+{
+  private String title;
+  private int rating;
+
+  public Song(String t)
+  {
+    title = t;
+    rating = 0;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public int getRating()
+  {
+    return rating;
+  }
+
+  public int adjustRating(int r)
+  {
+    int newRating = rating + r;
+    if (newRating >= 0 && newRating <= 10)
+    {
+      rating = newRating;
+    }
+    return rating;
+  }
+
+  public boolean equals(Song other)
+  {
+    return title.equals(other.title);
+  }
+
+  public String toString()
+  {
+    String info = "\"" + title + "\"";
+
+    if (rating != 0)
+    {
+      info += ", rating is " + rating;
+    }
+
+    return info;
+  }
+}
